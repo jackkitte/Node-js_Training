26d2772621d6317d117489a4774af20e17095acf
Tamashiro Kakeru <k-tamashiro@tads.co.jp>
2018-03-15 21:40:59 +0900

    N予備校 2017年度プログラミング入門Webアプリ サーバーサイドプログラミング入門 終了

--- node-js-http-3017/index.js
+++ node-js-http-3017/index.js
@@ -1,9 +1,23 @@
 'use strict';
 const http = require('http');
 const jade = require('jade');
-const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+const auth = require('http-auth');
+const basic = auth.basic(
+        {realm: 'Enter username and password.'},
+        (username, password, callback) => {
+            callback(username === 'guest' && password === 'xaxZJQmE');
+        });
+const server = http.createServer(basic, (req, res) => {
+  console.info('Requested by ' + req.connection.remoteAddress);
+
+  if (req.url === '/logout') {
+      res.writeHead(401, {
+          'Content-Type': 'text/plain; charset=utf-8'
+      });
+      res.end('ログアウトしました');
+      return;
+  }
+
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
@@ -38,7 +52,7 @@ const server = http.createServer((req, res) => {
       }).on('end', () => {
         body = Buffer.concat(body).toString();
         const decoded = decodeURIComponent(body);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info('投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="jp"><head><meta charset="utf-8"></head><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
@@ -49,11 +63,11 @@ const server = http.createServer((req, res) => {
   }
 
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error('Client Error', e);
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('Listening on ' + port);
 });
